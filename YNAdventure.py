71c94a86c369ed5ee37be639ada2076880aade0b
radly <danielpetranek@gmail.com>
2013-02-05 22:39:43 -0600

    added more branches

--- YNAdventure.py
+++ YNAdventure.py
@@ -41,7 +41,7 @@ def adventure0003():
         adventure0004()         
             
 def adventure0004():
-    print "When you're vision clears you see you are on a skyscraper with a train right next to you."
+    print "When you're vision clears you see you are on a skyscraper with a sky train right next to you."
     decision = raw_input("Do you board the train?")
     if decision == "yes" or decision == "y":
         print "Let's go somewhere sunny, you think. You hop on just as the doors are closing."
@@ -56,7 +56,7 @@ def adventure0005():
     print "As the train speeds away at a million kilomiles an hour, you realize you don't have a ticket. A man in a uniform is headed your way."
     decision = raw_input("Do you run for it?")
     if decision == "yes" or decision == "y":
-        print "You make your way away through the jumpsuited crowd as inconspicuously as possible."
+        print "You make your way away through the shiny jumpsuited crowd as inconspicuously as possible."
         #adventure0006()
     elif decision == "no" or decision == "n":
         print "What happens next?"
@@ -66,7 +66,7 @@ def adventure0005():
         
 def adventure0007():
     print "Just then, a gang of professional skateboarders enter through your window doing exotic and cool tricks."
-    decision = raw_input("We're to defeat King Eglon the Evil, bro! Join us?")
+    decision = raw_input("We're going to defeat King Eglon the Evil, bro! Join us?")
     if decision == "yes" or decision == "y":
         print "One of the skaters tosses you a sweet deck. You land on it in midair and do the best tricks."
         adventure0008()
@@ -78,14 +78,29 @@ def adventure0007():
             
 def adventure0008():
     print "In formation you do tricks down the street. King Eglon is waiting in the cul-du-sac."
-    decision = raw_input("Will you attempt to strike him righteously?")
+    decision = raw_input("Will you attempt to negotiate with him?")
     if decision == "yes" or decision == "y":
-        print "You attempt to try and hit him with a dogwalk 620 with a backside sickie, but he makes you biff!"
-        #adventure0009()
+        print "Eschewing violence, you skid to a halt and ask King Eglon to renounce his evil ways."
+        adventure0010()
+    elif decision == "no" or decision == "n":
+        print "You try to hit him with a dogwalk 620 with a backside sickie, but he makes you biff!"
+        adventure0010()
+    else:
+        print "It's a yes or no question, dude."
+    adventure0008() 
+
+def adventure0010():
+    print "He laughs in your face, then climbs aboard his rocket pogo stick and strikes down all the skaters. He then approaches you arrogantly. \"Join me and together we can rule the galaxy!\" he intones."
+    decision = raw_input("Will you join him?")
+    if decision == "yes" or decision == "y":
+        print "You stretch out your hand and he helps you up."
+        #adventure0011()
     elif decision == "no" or decision == "n":
         print "What happens next?"
     else:
         print "It's a yes or no question, dude."
-    adventure0008() 
-            
+    adventure0010()
+
+
+
 adventure0001()
